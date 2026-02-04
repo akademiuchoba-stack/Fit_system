6cf36c609862af889c5c85c4ecb6d5c82acceb4a
akademiuchoba <akademiuchoba@gmail.com>
2026-02-04 12:33:59 +0100

    ущё

--- backend/main.py
+++ backend/main.py
@@ -34,7 +34,7 @@ app.add_middleware(
 # ПУТИ
 # -----------------------------
 BASE_DIR = Path(__file__).resolve().parent.parent
-FRONTEND_DIR = BASE_DIR 
+FRONTEND_DIR = BASE_DIR / "frontend" 
 INDEX_FILE = FRONTEND_DIR / "index.html"
 
 # -----------------------------
